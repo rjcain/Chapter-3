ae7290db0e023d7d56f3527e6fffeef8ff76cb74
rjcain <31353104+rjcain@users.noreply.github.com>
2017-11-13 11:16:03 -0800

    Final Commit

---
+++ Tests.java
@@ -0,0 +1,13 @@
+
+public class Tests
+{
+   public static void main(String args[]) {
+       int i = 2;
+       System.out.println(i++);
+       System.out.println(i++);
+       i = 2;
+       System.out.println(++i);
+       i =2;
+       System.out.println(10 + (++i));
+    }
+}
